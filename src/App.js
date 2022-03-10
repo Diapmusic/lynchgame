7278f65cc9c2381325da171065861d38c3fe6add
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-10 11:51:27 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React  from 'react';
-<<<<<<< HEAD
+
 import { Footer, Possibility, Features, WhatGPT3, Header} from './containers';
-=======
 import { Footer,  Possibility, Features, WhatGPT3, Header} from './containers';
->>>>>>> f0b9b62b16a0c402807f67fa61c980f775781998
 import { CTA, Brand, Navbar } from './components';
 import './App.css';
 
@@ -24,10 +22,6 @@ function App (){
         <Features />
         <Possibility />
         <CTA />
-<<<<<<< HEAD
-      
-=======
->>>>>>> f0b9b62b16a0c402807f67fa61c980f775781998
         <Footer />
       </div>
   
