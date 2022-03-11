c558ee76667e7e5e9afe62e9f00ca65a8e64f535
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-11 17:44:57 +0100

    master

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -19,26 +19,20 @@ const Footer = () => (
         <img src={gpt3Logo} alt="gpt3_logo" />
         <p>Diamond-Studio, <br /> All Rights Reserved</p>
       </motion.div>
-      <motion.div className="gpt3__footer-links_div"
-        whileHover={{ scale: 1.01 }}
-        whileTap={{ scale: 0.8 }}
-      >
+      <div className="gpt3__footer-links_div">
         <h4>Social Media</h4>
         <p><a href='https://mobile.twitter.com/game_lynch'>Twitter</a></p>
         <p><a href='https://discord.gg/xAq9HZ55b5'>Discord</a></p>
         <p><a href='https://t.me/LynchOfficial'>Telegram</a></p>
         
-      </motion.div>
+      </div>
       
-      <motion.div className="gpt3__footer-links_div"
-        whileHover={{ scale: 1.01 }}
-        whileTap={{ scale: 0.8 }}
-      >
+      <div className="gpt3__footer-links_div">
         
         <h4>LYNCH-GAME</h4>
         
         <p>For Business Enquiries and Support Email: support@lynch.games</p>
-      </motion.div>
+      </div>
     </div>
 
     <div className="gpt3__footer-copyright">
