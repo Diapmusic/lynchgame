04e5df7af24ee8607910493c71dcbc692185240c
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-11 17:48:02 +0100

    master

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -4,21 +4,18 @@ import './footer.css';
 import { motion } from 'framer-motion/dist/framer-motion';
 const Footer = () => (
   <div className="gpt3__footer section__padding">
-    <motion.div className="gpt3__footer-heading"
+    <div className="gpt3__footer-heading"
       whileHover={{ scale: 1.01 }}
       whileTap={{ scale: 0.8 }}
     >
       <h1 className="gradient__text">Do you want to step in to the future before others</h1>
-    </motion.div>
+    </div>
 
     <div className="gpt3__footer-links">
-      <motion.div className="gpt3__footer-links_logo"
-       whileHover={{ scale: 1.01 }}
-       whileTap={{ scale: 0.8 }}
-      >
+      <div className="gpt3__footer-links_logo">
         <img src={gpt3Logo} alt="gpt3_logo" />
         <p>Diamond-Studio, <br /> All Rights Reserved</p>
-      </motion.div>
+      </div>
       <div className="gpt3__footer-links_div">
         <h4>Social Media</h4>
         <p><a href='https://mobile.twitter.com/game_lynch'>Twitter</a></p>
@@ -31,7 +28,7 @@ const Footer = () => (
         
         <h4>LYNCH-GAME</h4>
         
-        <p>For Business Enquiries and Support Email: support@lynch.games</p>
+        <p>For Business Enquiries and Support Email:<a href='support@lynch.games'> support@lynch.games</a></p>
       </div>
     </div>
 
