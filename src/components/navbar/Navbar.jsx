5dc37a060e0f1aaa5354ae146546848b5d3cc3c1
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-11 15:23:23 +0100

    master

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo3.png';
+import logo from '../../assets/lynch.png';
 import './navbar.css';
 
 
