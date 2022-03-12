0051b6595b3c8ecccf9d45d3d0b1860b2e2a5a7e
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-12 16:38:23 +0100

    master

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -16,7 +16,7 @@ const Possibility = () => (
      whileTap={{ scale: 0.8 }}
     >
       <h1 className="gradient__text">Lynch metaverse and VR EEG technology.</h1>
-      <p>The Lynch metaverse is no different from the real world. It is a completely realistic 3D virtual world where players and creators can be a part of it. The Metaverse is the future of each players’ real world, Players can create and own their imaginary virtual spirit realm and the real world.</p>
+      <p>The Lynch metaverse is no different from the real world. It is a completely realistic 3D virtual world where players and creators can be a part of it. The Metaverse is the future of each players Asset, Players can create and own their imaginary virtual spirit realm in the real world.</p>
       
     </motion.div>
   </div>
