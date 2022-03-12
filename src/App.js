81fba32c3387bdc85da690396ecbe057cc047316
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-12 14:51:42 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React , {useRef} from 'react';
+import React , {useState, useEffect} from 'react';
 import { Footer, Possibility, Features, WhatGPT3, Header} from './containers';
-
 import { CTA, Brand, Navbar } from './components';
+
 import './App.css';
 
 
@@ -11,19 +11,20 @@ function App (){
  
   return(
     <div className="App"  > 
-   
-      <div className="gradient__bg">
+     
+      
+          <div className="gradient__bg">
 
-        <Navbar />
-        <Header/>
-        <Brand />
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Footer />
-      </div>
-  
+            <Navbar />
+            <Header/>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Footer />
+          </div>
+       
   </div>
   );
 }
