f72ebc7e176910a0407e8800ad19f351419472e1
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-03-13 17:54:53 +0100

    master

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,14 +1,25 @@
 import React, {useState} from 'react';
 import './header.css';
 import {saveAs } from 'file-saver';
+import { init } from 'ityped';
+
 
 
 
 function Header (){
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current,{
+      showCursor: false,
+      backDelay:1500,
+      strings: [ "Lynch is a play-to-earn game", " play-to-win business model" , "it  enables active players to earn rewards" ,"  win and sell their rewards to other active players", " players will have full ownership of their in-game items", "In-game item can trade for real money " ]
+    }); 
+  },[]);
   const [show, setShow] = useState(false)
   const saveFile = () => {
     saveAs(
-      "https://github.com/Diapmusic/lynchgame/raw/master/src/assets/lynchWhitepaper.pdf",
+      "https://github.com/Diapmusic/lynchgame/raw/master/src/assets/lynchWhitepaper.pdf", 
       "example.pdf"
     );
   };
@@ -16,13 +27,8 @@ function Header (){
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content"> 
         <h1 className="gradient__text">We will build an RPG P2E metaverse Game for gamers and NFT addicts to play, earn and trade in a decentralized economy</h1>
-        <p>Lynch is a play-to-earn game</p>
+        <p><span ref={textRef} className="itypled-cursor"></span></p>
         <div className="gpt3__header-content__input">
-          
-        </div>
-        <div className="gpt3__header-content__input"
-       
-        >
           <button type="button" className='dw' onClick={saveFile}>Download Whitepaper</button>
         </div>
        
@@ -30,5 +36,6 @@ function Header (){
     </div>
   );
 }
+import { init } from 'ityped';
 
 export default Header;
