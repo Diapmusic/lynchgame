fe2e84f152df3748b6aedfde530f4d1ea1016d6e
Oluwadare Promise Emmanuel <95595405+Diapmusic@users.noreply.github.com>
2022-04-16 14:36:13 +0100

    master

--- src/containers/gallery/SliderData.js
+++ src/containers/gallery/SliderData.js
@@ -1,22 +1,26 @@
 export const SliderData = [
     {
       image:
-        'https://i.ibb.co/484wdNb/env1.png'
+        'https://i.ibb.co/b5cs3qw/tomasz-zero-day-06.jpg'
     },    
     {
       image:
-        'https://i.ibb.co/YLp4WjX/env2.png'
+        'https://i.ibb.co/Q6G30Y8/ly1.png'
     },
     {
       image:
-        'https://i.ibb.co/Ht8DVRH/env3.png'
+        'https://i.ibb.co/Qkf5XGc/tomasz-zero-day-01.jpg'
     },
     {
       image:
-        'https://i.ibb.co/yNn3gyM/env4.png'
+        'https://i.ibb.co/84Ld8wp/tomasz-zero-day-02.jpg'
     },
     {
       image:
-        'https://i.ibb.co/yNn3gyM/env4.png'
+        'https://i.ibb.co/Xt6ycYG/tomasz-zero-day-09.jpg'
+    },
+    {
+      image:
+        'https://i.ibb.co/PZ7Nghq/tomasz-zero-night-06.jpg'
     }
 ];
